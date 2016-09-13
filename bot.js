3560feb85e5ec3e75ecf052f0234c97d0a699d55
Jin Yan Ruan <jruan6377@bths.edu>
2016-09-13 00:56:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,8 +36,6 @@ const actions = {
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
       FB.fbMessage(recipientId, message, (err, data) => {
-        console.log(firstEntityValue(entities, "emotion"));
-        alert(firstEntityValue(entities, "emotion"));
         if (err) {
           console.log(
             'Oops! An error occurred while forwarding the response to',
