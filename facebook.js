7d22a18d77a4d4f25e64b0644fc9af43be9d2a77
Jin Yan Ruan <jruan6377@bths.edu>
2016-09-13 00:37:49 -0400

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -25,7 +25,7 @@ const fbMessage = (entities, recipientId, msg, cb) => {
         id: recipientId,
       },
       message: {
-        text: entities,
+        text: msg + entities,
       },
     },
   };
